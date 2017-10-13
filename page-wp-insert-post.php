16cb328bd229710e423b84d87d0b7cc542ceef2a
mazey <mazey@mazey.net>
2017-10-13 18:15:19 +0800

    Add remote post

--- page-wp-insert-post.php
+++ page-wp-insert-post.php
@@ -1,5 +1,4 @@
 <?php
-require_once(dirname(__FILE__) . '/../../../../wp-load.php');
 // Create post object
 $my_post = array(
     'post_title'    => wp_strip_all_tags( $_POST['post_title'] ),
@@ -7,9 +6,9 @@ $my_post = array(
     /* 'post_title'    => wp_strip_all_tags( 'post_title测试接收' ),
     'post_content'  => 'post_content测试接收', */
     'post_status'   => 'publish',
-    'post_author'   => 1,/* ,
-    'post_category' => array( 8,39 ) */
-    'tags_input' => '白发,头发'
+    'post_author'   => 1,
+    'tags_input' => $_POST['post_tag'],
+    'post_category' => array( $_POST['post_category'] )
 );
 
 // Insert the post into the database
