13e7c01c796091de94f28272819e85829a59cc6e
mazey <mazey@mazey.net>
2017-10-13 23:59:26 +0800

    Complete category

--- page-wp-insert-post.php
+++ page-wp-insert-post.php
@@ -3,13 +3,14 @@
 $my_post = array(
     'post_title'    => wp_strip_all_tags( $_POST['post_title'] ),
     'post_content'  => $_POST['post_content'],
-    /* 'post_title'    => wp_strip_all_tags( 'post_title测试接收' ),
-    'post_content'  => 'post_content测试接收', */
-    'post_status'   => 'publish',
+    'post_status'   => $_POST['post_status'],
     'post_author'   => 1,
     'tags_input' => $_POST['post_tag'],
-    'post_category' => array( $_POST['post_category'] )
+    'post_category' => explode(',', $_POST['post_category'])
 );
+
+/* print_r($my_post);
+die(); */
 
 // Insert the post into the database
 echo wp_insert_post( $my_post );
