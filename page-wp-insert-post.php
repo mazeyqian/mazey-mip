6a0f9256a4d49608a1241621d81fba4d2b80474f
mazey <mazey@mazey.net>
2017-10-15 22:06:07 +0800

    Future post

--- page-wp-insert-post.php
+++ page-wp-insert-post.php
@@ -3,9 +3,10 @@
 $my_post = array(
     'post_title'    => wp_strip_all_tags( $_POST['post_title'] ),
     'post_content'  => $_POST['post_content'],
+    'post_date'     => $_POST['post_date'],
     'post_status'   => $_POST['post_status'],
     'post_author'   => 1,
-    'tags_input' => $_POST['post_tag'],
+    'tags_input'    => $_POST['post_tag'],
     'post_category' => explode(',', $_POST['post_category'])
 );
 
